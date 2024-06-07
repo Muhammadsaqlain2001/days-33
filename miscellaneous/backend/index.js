063582842aedee83d73e336d7640d98f3334813f
MuhammadSaqlian2001 <saqlainasghar2001@gmail.com>
2024-06-07 22:10:55 +0500

    post method handling

--- miscellaneous/backend/index.js
+++ miscellaneous/backend/index.js
@@ -2,14 +2,18 @@ const express = require('express');
 const app = express();
 const port = 5000;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.get('/register', (req, res) => {
-    let {username, password} = req.query;
+  let { username, password } = req.query;
   res.send(`standad Get Response ${username}`)
-  console.log(`password ${password}`)
+
 })
 app.post('/register', (req, res) => {
-    res.send(`standad Post Response `)
-  })
+  let { username, password } = req.body
+  res.send(`standad Post Response ${username} `)
+})
 
 app.listen(port, () => {
   console.log(` listening on port ${port}`)
